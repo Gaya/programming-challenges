1b4e18bc223ee6fc3a6e69da3408f998bee9bfd0
Gaya Kessler <gaya.kessler@gmail.com>
2014-09-12 14:07:03 +0200

    Other answer

--- smallest-multiple/index.js
+++ smallest-multiple/index.js
@@ -4,16 +4,16 @@ var i = 1;
 while (found !== true) {
     var fail = false;
 
-    for (var j = 1; j < 21 && fail !== true; j++) {
+    for (var j = 1; j < 21; j++) {
         if (i % j !== 0) {
             fail = true;
+            i++;
+            break;
         }
     }
 
     if (fail === false) {
         found = true;
-    } else {
-        i++;
     }
 }
 

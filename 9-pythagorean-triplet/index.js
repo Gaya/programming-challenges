6a7c175134b60e30db36f8d85dca9ee5f4b654e1
Gaya <gaya.kessler@gmail.com>
2014-12-02 14:27:09 +0100

    euclid added

--- 9-pythagorean-triplet/index.js
+++ 9-pythagorean-triplet/index.js
@@ -12,4 +12,29 @@
 
 var outcome = 1000;
 
-console.log(Math.pow(outcome, 2));
+sumOfPytha(56, 33);
+
+function sumOfPytha(a, b) {
+    var s = euclid(a, b);
+    var c = pytha(s.a, s.b);
+    console.log(s.a, s.b, c, (s.a + s.b + c));
+}
+
+function exp(input) {
+    return Math.pow(input, 2);
+}
+
+function sqrt(input) {
+    return Math.sqrt(input);
+}
+
+function pytha(a, b) {
+    return sqrt(exp(a) + exp(b));
+}
+
+function euclid(m, n) {
+    return {
+        a: exp(m) - exp(n),
+        b: 2 * m * n
+    }
+}

54b8faca524b06e3f1a069db56423a2c8905d5b1
Gaya <gaya.kessler@gmail.com>
2014-12-18 16:54:13 +0100

    add problem 16

---
+++ 16-power-digit-sum/index.js
@@ -0,0 +1,9 @@
+/*
+ 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+ What is the sum of the digits of the number 2^1000?
+ */
+
+var pow = Math.pow(2, 10);
+
+console.log(pow);

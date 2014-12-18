a66e40814960eb2f808b0efbc9e9f98ad2d8556d
Gaya <gaya.kessler@gmail.com>
2014-12-18 16:25:25 +0100

    solved paths

--- 15-lattice-paths/index.js
+++ 15-lattice-paths/index.js
@@ -4,3 +4,20 @@
 
  How many such routes are there through a 20×20 grid?
  */
+
+function routesInGrid(x, y) {
+    var n = x + y;
+    var k = x;
+    var coeff = 1;
+    var i;
+
+    for (i = n-k+1; i <= n; i++) coeff *= i;
+    for (i = 1;     i <= k; i++) coeff /= i;
+
+    return Math.round(coeff);
+}
+
+console.log(routesInGrid(1, 1));
+console.log(routesInGrid(2, 2));
+console.log(routesInGrid(3, 3));
+console.log(routesInGrid(20, 20));

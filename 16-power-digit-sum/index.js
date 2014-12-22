fdc06f7ec46783d60152092d3f4b07ecb2ca6e25
Gaya <gaya.kessler@gmail.com>
2014-12-22 08:52:34 +0100

    solve with big int

--- 16-power-digit-sum/index.js
+++ 16-power-digit-sum/index.js
@@ -1,9 +1,16 @@
 /*
- 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
- What is the sum of the digits of the number 2^1000?
- */
+What is the sum of the digits of the number 2^1000?
+*/
 
-var pow = Math.pow(2, 10);
+var bigInt = require("big-integer");
+var pow = bigInt(2).pow(1000);
+var lol = pow.toString();
+var sum = 0;
 
-console.log(pow);
+for (var i = 0; i < lol.length; i++) {
+     sum += parseInt(lol[i]);
+}
+
+console.log(sum);
